6451071fc7a7b49ef4fe705601220eb2838c2711
AmareshB1312 <amareshbr89@gmail.com>
2026-04-02 15:41:17 +0530

    AmareshB1312 Added UC8: Linked List Based Palindrome Checker

--- Palindrome_CheckerApp/src/PalindromeCheckerApp.java
+++ Palindrome_CheckerApp/src/PalindromeCheckerApp.java
@@ -1,31 +1,72 @@
-import java.util.*;
+class ListNode {
+    char data;
+    ListNode next;
+
+    ListNode(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
 
 public class PalindromeCheckerApp {
+
+    // Function to check palindrome
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        // Step 1: Find middle using fast & slow pointers
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        ListNode prev = null, curr = slow;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        // Step 3: Compare both halves
+        ListNode firstHalf = head;
+        ListNode secondHalf = prev;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    // Helper to create linked list from string
+    public static ListNode createList(String str) {
+        ListNode head = null, tail = null;
+
+        for (char ch : str.toCharArray()) {
+            ListNode newNode = new ListNode(ch);
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         String input = "madam";
 
-        Deque<Character> deque = new ArrayDeque<>();
+        ListNode head = createList(input);
 
-        // Insert characters into deque
-        for (char ch : input.toCharArray()) {
-            deque.addLast(ch);  // insert at rear
-        }
-
-        boolean isPalindrome = true;
-
-        // Compare front and rear
-        while (deque.size() > 1) {
-            char front = deque.removeFirst(); // from front
-            char rear = deque.removeLast();   // from rear
-
-            if (front != rear) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        // Output result
-        if (isPalindrome) {
+        if (isPalindrome(head)) {
             System.out.println(input + " is a Palindrome");
         } else {
             System.out.println(input + " is NOT a Palindrome");
